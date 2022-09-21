186fbb8e0ac7ede5df4dcd012b14f3854cd0dea7
AndreyGOIT <ae.borga@gmail.com>
2022-09-21 12:07:01 +0300

    first commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+console.log(123);
+const gallery = document.querySelector(".gallery");
 
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}" >
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`;
+    })
+    .join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+
+new SimpleLightbox(".gallery a", {
+  captionsData: `alt`,
+  captionDelay: 250,
+  animationSpeed: 0,
+});
 console.log(galleryItems);
