fa48bfe516b32171a8bd43bdbd34377474654f91
AndreyGOIT <ae.borga@gmail.com>
2022-09-29 16:28:27 +0300

    HW8 task2 is also ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,54 +7,37 @@
 // 3) Инициализируй плеер в файле скрипта как это описано в секции
 // pre - existing player, но учти что у тебя плеер добавлен как npm пакет,
 //     а не через CDN.
+
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 // 4. Разбери документацию метода on() и начни отслеживать событие
 // timeupdate - обновление времени воспроизведения.
 const onPlay = function (data) {
   console.log('Идет воспроизведение видео');
-  // data is an object containing properties specific to that event
 };
 
 player.on('play', onPlay);
-
-player.on('timeupdate', function (data) {
-  {
-    duration: 61.857;
-    percent: 0.049;
-    seconds: 3.034;
-  }
-  // data is an object containing properties specific to that event
-});
-
 // 5. Сохраняй время воспроизведения в локальное хранилище.
 // Пусть ключом для хранилища будет строка "videoplayer-current-time".
+player.on('timeupdate', throttle(uppdatingTime, 1000));
+
+function uppdatingTime(data) {
+  const option = data.seconds;
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(option));
+}
 // 6. При перезагрузке страницы воспользуйся методом setCurrentTime()
 // для того чтобы возобновить воспроизведение с сохраненной позиции.
-player
-  .setCurrentTime(30.456)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+addEventListener('DOMContentLoaded', currentTime);
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+function currentTime() {
+  const timeForContinue = localStorage.getItem('videoplayer-current-time');
+  console.log(`CurrentTime is ${timeForContinue} sec`);
+  player.setCurrentTime(timeForContinue);
+}
+
 // 7. Добавь в проект бибилотеку lodash.throttle и сделай так,
 // чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
-document.addEventListener(
-  'timeupdate',
-  _.throttle(() => {
-    console.log('Timeupdate handler update every 1000ms');
-  }, 1000)
-);
